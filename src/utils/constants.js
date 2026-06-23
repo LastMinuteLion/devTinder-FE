4e8cd0fc9f5938c76872f47728a7ae921c561bd8
Harsh Vardhan <harsh20033@gmail.com>
2026-06-24 05:10:22 +0530

    yaya

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,2 @@
-// export const BASE_URL = "/api";
-
-
-
-export const BASE_URL =  "localhost" ?   "http://localhost:3000": "/api";
+export const BASE_URL =
+  location.hostname === "localhost" ? "http://localhost:7777" : "/api";
